8f1134edb4cc9f1894164f8dad2a68829c473db6
roksanaatapina-coder <roksanaatapina@gmail.com>
2026-03-24 03:05:20 -0400

    add mouseai/utils/__init__.py

---
+++ mouseai/utils/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+MouseAI Utils - Утилиты и вспомогательные функции
+"""
+
+from .helpers import MouseAIHelpers
+from .config import MouseAIConfig
+from .logger import MouseAILogger
+
+__all__ = [
+    'MouseAIHelpers',
+    'MouseAIConfig',
+    'MouseAILogger'
+]
